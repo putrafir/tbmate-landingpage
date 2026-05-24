d7f0c66cf1dc220d9d04def4bbf6ad18aaa73fac
putrafir <firdausputra0212@gmail.com>
2026-05-24 19:13:38 +0700

    ubah midleware jadi proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // Cek apakah user sedang mencoba mengakses area dashboard
   const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
 
